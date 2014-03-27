a20b98875cb08547b4c71b1f985d2e7c8ad84407
stevepm <steven.magelowitz@gmail.com>
2014-03-27 13:23:26 -0600

    Server set up with necessary gems

---
+++ spec/url_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+require 'capybara/rspec'
+require_relative '../url_shortener'
+Capybara.app = 'Url'
---
+++ url_shortener.rb
@@ -0,0 +1,5 @@
+require 'sinatra/base'
+
+class Url < Sinatra::Application
+
+end
