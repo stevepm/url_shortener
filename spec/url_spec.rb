ca277829887e6faf70b3d31272e117d14c31389f
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-03-27 15:50:40 -0600

    [Finished #68391602] User visits a shortened URL.
    Signed-off-by: Jenny Allar <jennyallar@gmail.com>

--- spec/url_spec.rb
+++ spec/url_spec.rb
@@ -4,6 +4,10 @@ require_relative '../url_shortener'
 Capybara.app = Url
 
 feature 'URL Shortener' do
+  background do
+    URLS_ARRAY = []
+  end
+
   scenario 'User goes to homepage' do
     visit '/'
     expect(page).to have_field('url')
@@ -15,12 +19,20 @@ feature 'URL Shortener' do
     fill_in('url', :with => "gschool.it")
     click_on('Shorten')
     expect(page).to have_content("gschool.it")
-    expect(page).to have_content("http://serene-atoll-7447.herokuapp.com/1")
+    expect(page).to have_content("www.example.com/1")
     visit '/'
     fill_in('url', :with => "hello.it")
     click_on('Shorten')
     expect(page).to have_content("hello.it")
-    expect(page).to have_content("http://serene-atoll-7447.herokuapp.com/2")
+    expect(page).to have_content("www.example.com/2")
+  end
+
+  scenario 'User can visit shortened URL' do
+    visit '/'
+    fill_in('url', :with => "google.com")
+    click_on('Shorten')
+    visit '/3'
+    expect(page.current_url).to eq('http://google.com/')
   end
 
 end
--- url_shortener.rb
+++ url_shortener.rb
@@ -2,7 +2,6 @@ require 'sinatra/base'
 require './model'
 
 class Url < Sinatra::Application
-  URLS = Url_Shortener.new
   URL_ARRAY = []
 
   get '/' do
@@ -10,10 +9,22 @@ class Url < Sinatra::Application
   end
 
   post '/url/add' do
-
     url = params[:url]
-    URL_ARRAY << [URLS.shorten(url), URL_ARRAY.count+1]
-    new_url = "http://serene-atoll-7447.herokuapp.com/"+URL_ARRAY[URL_ARRAY.count-1][1].to_s
+    URL_ARRAY << [url, URL_ARRAY.count+1]
+    new_url = request.host+"/"+URL_ARRAY[URL_ARRAY.count-1][1].to_s
     erb :shorten, :locals => {:url => url, :new_url => new_url}
   end
+
+  get '/:id' do
+    id = (params[:id].to_i) - 1
+
+    if URL_ARRAY.count == 0
+      redirect '/'
+    else
+      original_url = URL_ARRAY[id].first
+      redirect "http://#{original_url}"
+    end
+  end
+
+
 end
